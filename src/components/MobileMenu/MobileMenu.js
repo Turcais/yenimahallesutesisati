d3f93eeffdb0b4bd158e7440b3d7c03c96a5010b
Turcais <mrconest@gmail.com>
2024-12-06 02:47:48 +0300

    Modified files for feature update

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -8,176 +8,22 @@ import './style.css';
 const menus = [
     {
         id: 1,
-        title: 'Home',
+        title: 'Anasayfa',
         link: '/home',
-        submenu: [
-            {
-                id: 11,
-                title: 'Home style 1',
-                link: '/home-1'
-            },
-            {
-                id: 12,
-                title: 'Home style 2',
-                link: '/home-2'
-            },
-            {
-                id: 13,
-                title: 'Home style 3',
-                link: '/home-3'
-            }
-        ]
+       
     },
 
-    {
-        id: 6,
-        title: 'Event',
-        link: '/event',
-        submenu: [
-            {
-                id: 61,
-                title: 'Event 1',
-                link: '/event',
-            },
-            {
-                id: 61,
-                title: 'Event 2',
-                link: '/event-s2',
-            },
-            {
-                id: 62,
-                title: 'Event single',
-                link: '/event-single/Exploring-Islamic-Insights-and-Wisdom'
-            }
-        ]
-    },
+    
     {
         id: 7,
-        title: 'Service',
-        link: '/service',
-        submenu: [
-            {
-                id: 71,
-                title: 'Service 1',
-                link: '/service',
-            },
-            {
-                id: 71,
-                title: 'Service 2',
-                link: '/service-s2',
-            },
-            {
-                id: 72,
-                title: 'Service Single',
-                link: '/service-single/Quran-Memorization'
-            }
-        ]
+        title: 'Hizmetler',
+        link: '/service-s2',
+       
     },
-    {
-        id: 8,
-        title: 'Shop',
-        link: '/shop',
-        submenu: [
-            {
-                id: 81,
-                title: 'Shop',
-                link: '/shop',
-            },
-            {
-                id: 82,
-                title: 'Shop Single',
-                link: '/product-single/Perfume'
-            },
-            {
-                id: 83,
-                title: 'Cart',
-                link: '/cart'
-            },
-            {
-                id: 84,
-                title: 'Checkout',
-                link: '/checkout'
-            },
-        ]
-    },
-    {
-        id: 3,
-        title: 'Pages',
-        link: '/',
-        submenu: [
-            {
-                id: 31,
-                title: 'About',
-                link: '/about'
-            },
-            {
-                id: 32,
-                title: 'Donate',
-                link: '/donate'
-            },
-            {
-                id: 33,
-                title: 'Login',
-                link: '/login'
-            },
-            {
-                id: 34,
-                title: 'Register',
-                link: '/register'
-            },
-            {
-                id: 5,
-                title: 'forgot password',
-                link: '/forgot'
-            },
-            {
-                id: 6,
-                title: '404',
-                link: '/404'
-            },
-        ]
-    },
-
-    {
-        id: 5,
-        title: 'Blog',
-        link: '/blog',
-        submenu: [
-            {
-                id: 51,
-                title: 'Blog',
-                link: '/blog'
-            },
-            {
-                id: 52,
-                title: 'Blog Left sidebar',
-                link: '/blog-left'
-            },
-            {
-                id: 53,
-                title: 'Blog full width',
-                link: '/blog-fullwidth'
-            },
-            {
-                id: 54,
-                title: 'Blog single',
-                link: '/blog-single/The-Importance-Of-Marriage-In-Islam.'
-            },
-            {
-                id: 55,
-                title: 'Blog single Left sidebar',
-                link: '/blog-single-left-sidebar/The-Importance-Of-Marriage-In-Islam.'
-            },
-            {
-                id: 56,
-                title: 'Blog single Left sidebar',
-                link: '/blog-single-fullwidth/The-Importance-Of-Marriage-In-Islam.'
-            },
-        ]
-    },
+    
     {
         id: 88,
-        title: 'Contact',
+        title: 'İletişim',
         link: '/contact',
     }
 
